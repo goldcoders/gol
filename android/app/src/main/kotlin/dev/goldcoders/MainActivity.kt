9983a5066792c04d119592427ce201b4688323f4
Uriah <goldcoders@protonmail.com>
2023-03-03 07:03:10 +0800

    init

---
+++ android/app/src/main/kotlin/dev/goldcoders/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.goldcoders.gol
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:gol/app/app.dart';
+import 'package:gol/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
